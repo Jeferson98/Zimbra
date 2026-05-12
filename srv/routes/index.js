3b0e7102a81b358079a25be85209da85ed46172a
Jef Mesa <jefersonmesa98@gmail.com>
2026-05-12 08:45:16 -0500

    Clear Backend

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -2,9 +2,5 @@
 const router = require('express').Router();
 
 router.use('/SedesMaestro', require('./Sedes'));
-router.use('/ProductosMaestro', require('./Productos'));
-router.use('/UsuariosMaestro', require('./Usuarios'));
-router.use('/StockMaestro', require('./Stock'));
-router.use('/SalesOrder', require('./SalesOrder'));
 
 module.exports = router;
