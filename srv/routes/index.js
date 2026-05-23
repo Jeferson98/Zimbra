446a4eb42a67fd7cecd740f11bcf713c27b7f429
Jef Mesa <jefersonmesa98@gmail.com>
2026-05-23 12:10:13 -0500

    Add Archivos a index

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -5,9 +5,19 @@ const router = require('express').Router();
 router.use('/contactos', require('./contactos'));
 router.use('/destinatarios', require('./destinatarios'));
 */
+router.use('/Archivos', require('./archivos'));
+router.use('/Campanas', require('./campanas'));
+router.use('/Correos', require('./correos'));
+router.use('/Demo', require('./demo'));
+router.use('/Emails_enviados', require('./emails_enviados'));
+router.use('/Envio_formularios', require('./envio_formularios'));
+router.use('/Eventos', require('./eventos'));
+router.use('/Formularios', require('./formularios'));
+router.use('/Leads', require('./leads'));
+router.use('/Notas', require('./notas'));
+router.use('/Notificaciones', require('./notificaciones'));
+router.use('/Reglas_scoring', require('./Reglas_scoring'));
+router.use('/Score_Historial', require('./Score_historial'));
 router.use('/Usuarios', require('./usuario'));
-router.use('/eventos', require('./eventos'));
-router.use('/archivos', require('./archivos'));
-router.use('/correos', require('./correos'));
 
 module.exports = router;
