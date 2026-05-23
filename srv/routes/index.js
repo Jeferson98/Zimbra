98007973eb27ad4646b845348666b6c2eafe04b6
Jef Mesa <jefersonmesa98@gmail.com>
2026-05-22 23:48:10 -0500

    add archivos backend

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -1,6 +1,13 @@
 // routes/index.js
 const router = require('express').Router();
 
-router.use('/SedesMaestro', require('./Sedes'));
+/*router.use('/archivos', require('./archivos'));
+router.use('/contactos', require('./contactos'));
+router.use('/correos', require('./correos'));
+router.use('/destinatarios', require('./destinatarios'));
+router.use('/eventos', require('./eventos'));
+router.use('/mensajes', require('./mensajes'));
+router.use('/roles', require('./roles'));
+*/router.use('/Usuarios', require('./usuario'));
 
 module.exports = router;
